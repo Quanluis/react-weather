26443f8f21ae00069576a201df4576a5e33394c4
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-21 11:36:40 -0500

    added a set state for initial lat and lot as well as binds to handle input values

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(){
+    super();
+    this.setState = {
+     lat: 0,
+     lon: 0 
+    };
+    this.handleLatChange = this.handleLatChange.bind(this);
+    this.handleLonChange = this.handleLonChange.bind(this);
+
+  }
+  handleLatChange(e){
+    this.setState({
+      lat: +e.target.value 
+    })
+  }
+  handleLonChange(e){
+    this.setState({
+      lon: +e.target.value
+    })
+  }
   render() {
     return (
      <div>
@@ -9,11 +31,14 @@ class App extends Component {
       <p>Enter your latitude and longitude</p>
       <form>
         <label>
+          latitude
           <input type="text"/>
         </label>
         <label >
+          longitude
           <input type="text"/>
         </label>
+        <button type = 'submit' >Get the Weather!</button>
       </form>
      </div>
     );
