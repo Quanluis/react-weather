02401993d1036123b2ad99bd43450202dba8b042
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-22 18:26:35 -0500

    commited weather service that lets me get weather data from the api additional updated handle submit to handle the weather

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import "./App.css";
+import { getWeather } from "./services/weather";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       lat: 0,
       lon: 0,
+      currentWeather: {}
     };
     this.handleLatChange = this.handleLatChange.bind(this);
     this.handleLonChange = this.handleLonChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleLatChange(e){
+  handleLatChange(e) {
     this.setState({
       lat: +e.target.value
-    })
+    });
   }
-  handleLonChange(e){
+  handleLonChange(e) {
     this.setState({
       lon: +e.target.value
-    })
+    });
   }
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
-    console.log('Hello world');
-    axios.get('/forecast/0,0')
-    .then(response => console.log(response));
-    
+    console.log("Hello world");
+
+    getWeather(this.state.lat, this.state.lon)
+      .then(response => {
+        const currentWeather = response.data.currenntly;
+        this.setState({
+          currentWeather: currentWeather
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   render() {
@@ -37,27 +45,32 @@ class App extends Component {
       <div>
         <h1>React Weather</h1>
         <p>Enter your latitude and longitude to get the weather!</p>
-        <form onSubmit = {(e) => this.handleSubmit(e)}>
+        <form onSubmit={e => this.handleSubmit(e)}>
           <label>
             Latitude:
-            <input type="number" 
-              min = '-90'
-              max = '90'
-              onChange={(e) => this.handleLatChange(e)} 
+            <input
+              type="number"
+              min="-90"
+              max="90"
+              onChange={e => this.handleLatChange(e)}
               value={this.state.lat}
-              required/>
+              required
+            />
           </label>
           <label>
             Longitude:
-            <input type="number" 
-            min = '-180'
-            max = '180'
-              onChange={(e) => this.handleLonChange(e)} 
+            <input
+              type="number"
+              min="-180"
+              max="180"
+              onChange={e => this.handleLonChange(e)}
               value={this.state.lon}
-              required/>
+              required
+            />
           </label>
           <button type="submit">Get The Weather!</button>
         </form>
+        <pre>{JSON.stringify(this.state.currentWeather, null, 4)}</pre>
       </div>
     );
   }
