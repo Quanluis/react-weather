3bec22d24e26da99cd2e2f28c31bd57a531bcbc9
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-22 20:36:39 -0500

    setup image selections for weather app

---
+++ client/src/image/index.js
@@ -0,0 +1,14 @@
+import rain from './cloud.rain.png';
+
+const selection = (name) => {
+    switch(name){
+    case 'rain':
+        return rain;
+    default:
+        return rain;
+    }
+    
+}
+
+export default selection;
+
